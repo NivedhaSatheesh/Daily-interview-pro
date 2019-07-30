720d3f0b36c9e1b2685f6260cf3ad29ff306f550
NivedhaSatheesh <51219713+NivedhaSatheesh@users.noreply.github.com>
2019-07-30 11:44:51 -0700

    Create Problem 12.py

---
+++ Problem 12.py
@@ -0,0 +1,15 @@
+def isTriplet(ar, n): 
+    j = 0
+      
+    for i in range(n - 2): 
+        for k in range(j + 1, n): 
+            for j in range(i + 1, n - 1): 
+                # Calculate square of array elements 
+                x = ar[i]*ar[i] 
+                y = ar[j]*ar[j] 
+                z = ar[k]*ar[k]  
+                if (x == y + z or y == x + z or z == x + y): 
+                    return 1
+      
+    # If we reach here, no triplet found 
+    return 0
